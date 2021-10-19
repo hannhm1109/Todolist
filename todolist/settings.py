3714e5d20d41f95196880eaec70d82159907d4ce
hanane1109 <hananenhm2000@gmail.com>
2021-10-19 13:54:26 +0000

    changed database from sqlite3 to postgres

--- todolist/settings.py
+++ todolist/settings.py
@@ -76,8 +76,18 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'todolistdb',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'Dondaxplanether22',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
 }
 
